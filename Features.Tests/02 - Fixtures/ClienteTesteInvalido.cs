771b119c8bfceb2787e1952bb2c2d8021ea1905b
Gildeir L. Rodrigues <gildeirlopes@gmail.com>
2024-12-01 18:57:01 -0300

    Add test for valid client creation using fixture

---
+++ Features.Tests/02 - Fixtures/ClienteTesteInvalido.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteCollection))]
+    public class ClienteTesteInvalido
+    {
+        private readonly ClienteTestsFixture _clienteTestsFixture;
+
+        public ClienteTesteInvalido(ClienteTestsFixture clienteTestsFixture)
+        {
+            _clienteTestsFixture = clienteTestsFixture;
+        }
+
+        [Fact(DisplayName = "Novo Cliente Nome Inválido")]
+        [Trait("Categoria", "Cliente Fixture Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = _clienteTestsFixture.GerarClienteInValidoByName();
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEmpty(cliente.ValidationResult.Errors);
+        }
+        
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Fixture Testes")]
+        public void Cliente_NovoCliente_DeveEstarEmailInvalido()
+        {
+            // Arrange
+            var cliente = _clienteTestsFixture.GerarClienteInValidoByEmail();
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEmpty(cliente.ValidationResult.Errors);
+        }
+    }
+}
---
+++ Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,59 @@
+﻿using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>
+    { }
+
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Matheus",
+                "Oliveira",
+                DateTime.Now.AddYears(-20),
+                "mathreis1999@gmail.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInValidoByName()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "gildeirlopes@gmail.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        } 
+        
+        public Cliente GerarClienteInValidoByEmail()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "gildeirlopes@gmail.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
