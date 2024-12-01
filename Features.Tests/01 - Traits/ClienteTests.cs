df97299a5849613ab14d51a7cf9738c91e02952b
Gildeir L. Rodrigues <gildeirlopes@gmail.com>
2024-12-01 18:07:48 -0300

    feature/ adding client

---
+++ Features.Tests/01 - Traits/ClienteTests.cs
@@ -0,0 +1,54 @@
+﻿using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName= "Novo Cliente Valido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Gildeir",
+                "Rodrigues",
+                DateTime.Now.AddYears(-20),
+                "gildeirlopes@gmail.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Empty(cliente.ValidationResult.Errors);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Invalido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "mathreis1999@gmail.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEmpty(cliente.ValidationResult.Errors);
+        }
+    }
+}
