b60a2353fec643b576b846d7790a9933bed1da63
Gildeir L. Rodrigues <gildeirlopes@gmail.com>
2024-12-14 17:21:07 -0300

    Adding pedido command handler tests and application layer

---
+++ NerdStore.Vendas.Application.Teste/Pedidos/AdicionarItemPedidoCommandTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Application.Teste.Pedidos
+{
+    public class AdicionarItemPedidoCommandTests
+    {
+        [Fact(DisplayName = "Adicionar Item Command Válido")]
+        [Trait("Categoria", "Vendas - Pedido Command")]
+        public void AdicionarItemPedidoCommand_ComandoEstaValido_DevePasssarNaValidacao()
+        {
+            //Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.NewGuid(), Guid.NewGuid(), "Produto Teste", 2, 100);
+
+            //Act
+            var result = pedidoCommand.EhValido();
+
+            //Assert
+            Assert.True(result);
+        }
+
+    }
+}
